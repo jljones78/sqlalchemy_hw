7e3974b0bab4c6f0925c8021b16909498b7136b0
jljones78 <jljones78@gmail.com>
2020-02-15 13:48:03 -0600

    solved through tobs

--- climate.py
+++ climate.py
@@ -1,5 +1,6 @@
 import datetime as dt
 import pandas as pd
+import numpy as np
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
@@ -7,7 +8,7 @@ from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
 
 # set up db and create session
-engine = create_engine("sqlite:///hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 Measurement = Base.classes.measurement
@@ -17,16 +18,63 @@ session = Session(engine)
 # initialize flask
 app = Flask(__name__)
 
-
-# create routes
+# define routes
 @app.route("/")
+
 # return all available endpoints
 def home():
     return (
-        f"endpoints avilable:<br/>"
+        f"available endpoints:<br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
         f"/api/v1.0/'start-date'<br/>"
         f"/api/v1.0/'start-date'/'end-date'<br/>"
     )
+
+
+@app.route("/api/v1.0/precipitation")
+# convert the query results to a dict and return as json
+def precipitation():
+    prcp = (
+        session.query(Measurement.prcp, Measurement.date)
+        .order_by(Measurement.date)
+        .all()
+    )
+    prcp_df = pd.DataFrame(prcp).dropna()
+    precip_dict = prcp_df.to_dict()
+    Session.close(session)
+    return jsonify(precip_dict)
+
+
+@app.route("/api/v1.0/stations")
+# return a json list of stations from the dataset
+def stations():
+    stations = session.query(Measurement.station).group_by(Measurement.station).all()
+    stations_df = pd.DataFrame(stations)
+    stations_dict = stations_df.to_dict()
+    Session.close(session)
+    return jsonify(stations_dict)
+
+
+@app.route("/api/v1.0/tobs")
+# query for the dates and temperature observations from a year from the last data point
+# return a json list of the tobs for the previous year
+def tobs():
+    last_date = session.query(func.max(Measurement.date)).all()[0][0]
+    last_date_dt = dt.datetime.strptime(last_date, "%Y-%m-%d")
+    first_date_dt = last_date_dt - dt.timedelta(days=365)
+    last12_temp = (
+        session.query(Measurement.tobs, Measurement.date)
+        .filter(Measurement.date > first_date_dt)
+        .order_by(Measurement.date)
+        .all()
+    )
+    last12_temp_df = pd.DataFrame(last12_temp).dropna().set_index("date")
+    tobs_dict = last12_temp_df.to_dict()
+    Session.close(session)
+    return jsonify(tobs_dict)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
